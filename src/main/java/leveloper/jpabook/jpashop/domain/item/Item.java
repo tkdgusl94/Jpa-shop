78c0ba35a607970fc5ffd40215b8ff4b80dd4999
tkdgusl94 <tkdgusl94@gmail.com>
2020-01-28 16:31:22 +0900

    item service, repository code 작성

--- src/main/java/leveloper/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/leveloper/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package leveloper.jpabook.jpashop.domain.item;
 
 import leveloper.jpabook.jpashop.domain.Category;
+import leveloper.jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * stock 재고 중가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * sotck 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ src/main/java/leveloper/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package leveloper.jpabook.jpashop.repository;
+
+import leveloper.jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null)
+            em.persist(item); // 신규로 등록한다.
+        else
+            em.merge(item); // 업데이트
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
